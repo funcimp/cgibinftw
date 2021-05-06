e6b976fef19dcd7e86439b1d4984ad69fac363e3
Nathan Toups <n@rjrbt.io>
2021-05-05 23:19:54 -0600

    added options for asset render

--- Makefile
+++ Makefile
@@ -1,14 +1,11 @@
 
-
-
 build:
 	go build -o dist/cgi-bin/ulticntr ulticntr/*.go
 
 run: build
 	go run main.go
 
-# dev uses enter in reload mode to watch changes in go files and template files 
+# dev uses enter in reload mode to watch changes in go files and template files
 # to rebuild ulticntr and restart the cgi server
 dev:
-	ls ./*.go ulticntr/*.go ulticntr/assets/*.go.html | \
-	entr -r make run
+	ls ./*.go ulticntr/*.go ulticntr/assets/*.go.html | entr -r make run
--- ulticntr/main.go
+++ ulticntr/main.go
@@ -6,16 +6,18 @@ import (
 	"encoding/base64"
 	"fmt"
 	"html/template"
+	"log"
+	"math/rand"
+	"time"
 )
 
 var (
 	//go:embed assets/html.go.html
 	htmlTemplate string
-)
-
-var (
 	//go:embed assets/images/starbg.gif
 	starBG []byte
+	//go:embed assets/images/fireworksbg.gif
+	fireworksBG []byte
 	//go:embed assets/images/alien.gif
 	alien []byte
 	//go:embed assets/images/disk.gif
@@ -30,43 +32,72 @@ var (
 
 func main() {
 	if err := render(); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 }
 
 type img struct {
 	data   []byte
+	Class  string
 	Width  uint
 	Height uint
 }
 
 func (i img) URI() template.URL {
-	encoded := base64.StdEncoding.EncodeToString(i.data)
-	return template.URL(fmt.Sprintf("data:image/gif;base64,%v", encoded))
+	uri := fmt.Sprintf("data:image/gif;base64,%v", base64.StdEncoding.EncodeToString(i.data))
+	return template.URL(uri)
+}
+
+func (a assets) GetClassName(i int) string {
+	return a.Images[a.Arrangement[i]].Class
 }
 
 type assets struct {
-	Background img
-	Images     map[string]img
-	Alien      img
-	Disk       img
-	Spidey     img
-	Yinyang    img
-	Peace      img
-	Counter    uint64
+	Background  img
+	Images      []img
+	Arrangement []uint
+	Counter     uint64
 }
 
-func newAssets() assets {
-	images := make(map[string]img)
-	images["alien"] = img{data: alien, Width: 82, Height: 90}
-	images["disk"] = img{data: disk, Width: 65, Height: 80}
-	images["peace"] = img{data: peace, Width: 74, Height: 75}
-	images["yinyang"] = img{data: yinyang, Width: 65, Height: 65}
-	images["spidey"] = img{data: spidey, Width: 241, Height: 124}
+type option func(*assets)
 
-	return assets{
-		Images:     images,
-		Background: img{data: starBG}}
+func withFireworks() option {
+	return func(a *assets) {
+		a.Background = img{data: fireworksBG}
+	}
+}
+
+func withRandomImages() option {
+	return func(a *assets) {
+		rand.Seed(time.Now().UnixNano())
+		min := 1
+		max := 4
+		arr := []uint{0}
+		for i := 1; i <= 12; i++ {
+			arr = append(arr, uint(rand.Intn(max-min+1)+min))
+		}
+		a.Arrangement = arr
+	}
+}
+
+func newAssets(options ...option) assets {
+	images := []img{
+		{data: spidey, Class: "spidey", Width: 241, Height: 124},
+		{data: alien, Class: "alien", Width: 82, Height: 90},
+		{data: disk, Class: "disk", Width: 65, Height: 80},
+		{data: peace, Class: "peace", Width: 74, Height: 75},
+		{data: yinyang, Class: "yinyang", Width: 65, Height: 65},
+	}
+	arr := []uint{0, 1, 3, 2, 1, 4, 1, 1, 4, 1, 2, 3, 1}
+	a := assets{
+		Images:      images,
+		Arrangement: arr,
+		Background:  img{data: starBG},
+	}
+	for _, opt := range options {
+		opt(&a)
+	}
+	return a
 }
 
 func render() error {
